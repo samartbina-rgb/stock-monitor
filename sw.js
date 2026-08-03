4c3cfd743dd606e99a6b145039c23eaa1f719ee3
samartbina-rgb <samartbina@gmail.com>
2026-08-03 19:08:12 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "compare-app-v6";
+const CACHE_NAME = "compare-app-v7";
 const SHELL_FILES = ["./", "./index.html", "./manifest.json", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (event) => {
